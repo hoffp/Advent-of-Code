f0cc8ca9abe19b7ef99b5fb4f639f03a4337b751
Peter Hoff <hoffp@student.chalmers.se>
2022-12-20 16:29:33 +0100

    day4 complete

--- 2022/day4/day4.py
+++ 2022/day4/day4.py
@@ -1,24 +1,43 @@
 class Elf:
-    start = 0
-    end = 0
+    min = 0
+    max = 0
+    sect_cover = []
 
-    def __init__(self, s, e) -> None:
-        self.start = s
-        self.end = e
+    def __init__(self, sections) -> None:
+
+        self.min = int(sections.split("-")[0])
+        self.max = int(sections.split("-")[1])
+        self.sect_cover = range(self.min, self.max + 1)
+
+    def contains(self, elf):
+        return elf.min <= self.min and self.max <= elf.max
+
+    def overlaps(self, elf):
+        return not set(self.sect_cover).isdisjoint(set(elf.sect_cover))
+
+elf_list = []
+
+def part_two(data):
+    count = 0
+    for pair in elf_list:
+        if pair[0].overlaps(pair[1]):
+            count += 1
+    print(count)
+
 
 def part_one(data):
-    elves = [Elf(y[0].split("-")[0], y[1].split("-")[1]) for y in [x.split(",") for x in data.splitlines()]]
-    print(elves)
-    # for sections in data:
-    #     for elf in sections:
-    #         elf = elf.split("-")
-    #         print(elf[0] + " and " + elf[1])
-    #         e = Elf(elf[0], elf[1])
-    #         elves.append(e)
-        
+    count = 0
+    for pair in data.splitlines():
+        elves = pair.split(",")
+        e1 = Elf(elves[0])
+        e2 = Elf(elves[1])
+        elf_list.append((e1,e2))
+        if e1.contains(e2) or e2.contains(e1):
+            count += 1
 
-    #print(data)
+    print(count)
 
 if __name__ == "__main__":
     data = open("input").read()
     part_one(data)
+    part_two(data)
