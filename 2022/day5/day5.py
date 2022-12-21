1aa8d05874c72eca71b68ad2155ba7606f77e8f8
Peter Hoff <hoffp@student.chalmers.se>
2022-12-21 17:46:48 +0100

    day5 part one complete

--- 2022/day5/day5.py
+++ 2022/day5/day5.py
@@ -8,34 +8,36 @@ class Stack:
         return str(["["+str(c)+"]" for c in self.crates])
 
     def add_crate(self, c):
-        self.crates.append(c)
+        self.crates.insert(0,c)
 
-class Crate:
-    contents = ''
-
-    def __init__(self, c) -> None:
-        self.contents = c
-
-    def __str__(self) -> str:
-        return self.contents
-
-    def move(self, old, new):
-        old.remove_crate(self)
-        new.add_crate(self)
+    def move_crate(self, s):
+        s.add_crate(self.crates.pop(0))
 
 stacks = [Stack(), Stack(), Stack(), Stack(), Stack(), Stack(), Stack(), Stack(), Stack()]
 
 def part_one(data):
-    start_config = data.split("\n\n")[0]
+    data = data.split("\n\n")
 
-    for row in start_config.splitlines():
+    start_config = data[0].splitlines()
+    for row in reversed(start_config):
         stack_index = 0
         for n in range(1, len(row), 4):
             if row[n].isalpha():
-                stacks[stack_index].add_crate(Crate(row[n]))
+                stacks[stack_index].add_crate(row[n])
             stack_index += 1
     
-    move_set = data.split("\n\n")[1]
+    move_set = data[1].splitlines()
+    for move in move_set:
+        move = move.split(" ")
+
+        number_of_crates = int(move[1])
+        from_stack = stacks[int(move[3]) - 1]
+        to_stack = stacks[int(move[5]) - 1]
+
+        for n in range(0, number_of_crates):
+            from_stack.move_crate(to_stack)
+        
+    print(''.join([s.crates[0] for s in stacks]))
     
 if __name__ == "__main__":
     data = open("input").read()
